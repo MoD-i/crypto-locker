2935582d8e4cc9bbb6e4b17e14498bb359ed1cc2
Aditya Prasad <aytida77@gmail.com>
2016-10-03 19:23:54 +0530

    readme and doc

--- src/main/java/com/psiphiglobal/proto/TestHarness.java
+++ src/main/java/com/psiphiglobal/proto/TestHarness.java
@@ -1,6 +1,5 @@
 package com.psiphiglobal.proto;
 
-import com.psiphiglobal.proto._core.Constants;
 import com.psiphiglobal.proto.blockchain.impl._core.JsonRpcClient;
 import com.psiphiglobal.proto.blockchain.impl._core.JsonRpcException;
 import com.psiphiglobal.proto.blockchain.impl.request.PublishToStreamRequest;
@@ -13,14 +12,15 @@ import com.psiphiglobal.proto.model.DocumentSummary;
 import com.psiphiglobal.proto.model.User;
 import com.psiphiglobal.proto.model.helper.DocumentHelper;
 import com.psiphiglobal.proto.model.helper.UserHelper;
-import com.psiphiglobal.proto.util.GsonProvider;
 import com.psiphiglobal.proto.util.crypto.AsymmetricCryptoUtil;
+import com.psiphiglobal.proto.util.crypto.CryptoException;
 import com.psiphiglobal.proto.util.crypto.SymmetricCryptoUtil;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,17 +29,22 @@ public class TestHarness
 {
     private static JsonRpcClient jsonRpcClient;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        JsonRpcClient.init(GsonProvider.get(), Constants.BLOCKCHAIN_RPC_URL, Constants.BLOCKCHAIN_RPC_USERNAME, Constants.BLOCKCHAIN_RPC_PASSWORD);
-        jsonRpcClient = JsonRpcClient.getInstance();
-
-        getSharedDocuments(jsonRpcClient);
+//        String to = "gaurav";
+//        String docId = "a2581b62839c4bc8c05847bba6ccde55294da12a7928fca87e4f1d901749660a";
+//        String encKey = "lC8IPZEzdS5PA8aVFPiaR5g/P2YUYi/9U6lWIt7wMKugbq3/WOhnldx8Ihwi6UZJm+en/JI/aac15DAJyGeiiF6tuCZbduDJrksYErdLKd7a9ynQMSJ0aUiQlZzXqGRTsorv5e1cIdBY/d9N5/1kAKeL4yt6RxKaYILhSkHuiMQ=";
+//        String raw = to + "|" + docId + "|" + encKey;
+//
+//        String privateKey = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIsNHRMy907p60/fg6+mjB4Dc1ZJv+HTrsD0apvLbjyhZpQ17J5ikcO0GlnpXptQW9wV8kOqkyKXrPSjT0KXwfSxsbfgYLEovJgZB93QTt9aAYgrM54BL2k67qj70lmq1bBPQamlUfGrzVWpl1yZgWYRdUYfy+HmQD5SEUn0ATcHAgMBAAECgYB3RK88qYmQ6XmJ0Cjuv8cNc145lBZ+yAfrv3m12f7bCIGo/LvRerCWkWPM3Svlwj6Yf0aGnceIc1rJl05Dy1Fzla7ji/xYizRUN3q3Pb4IPLIeF7FPMC8EXuMQ4eyF6SL4eplPzq05cAFaHlllLbldJF+s7ws2sFO9ev8OqBdVcQJBAPlDGUtIl2TvxWXhWhDZUikf7SJxyEZMgXfvZiIGru43cD088U/9TWs9ikYaMfsuB1S3McXLhx/XehGpt7fZKm8CQQCOz1opfX2pReMzQNMyXf5EHun4relE+saCEAtIFvJzrPK4iSbQ5aX9pi+8NIRXyghQsCNSe1y7GEq3o0Nz6+jpAkAbwNXTJQz+RxgihNnjYF8ehxh/Kk4TTbdopDYp+baGfZO4rc1si5aQJzT7IzWHcxOL4i3fiQ7Ng89qogv7o2lvAkBqWhUFLQV2GCvZMX+W1NTUmkkX/zvnOPK4TYZ5S02Hw9aGgu07SKrTOP33pQyh7D1BCctkU6Z9Vtvd4mILZDZxAkEAmfHnHgLLmP94gCOxot4Il8bPzpoHdoQqiK4D69m6d9B/G6unpLVACBpEDRsQDBD2djDQLEBBKHdkQHUn8q19gw==";
+//        byte[] sign = AsymmetricCryptoUtil.sign(Base64.decodeBase64(privateKey), raw.getBytes());
+//        System.out.println(Base64.encodeBase64String(sign));
+        oldmethod();
     }
 
     private static void oldmethod() throws Exception
     {
-        File file = new File("/Users/harsh/Downloads/bitcoin.pdf");
+        File file = new File("/Users/aditya/bitcoin.pdf");
         byte[] contents = IOUtils.toByteArray(new FileInputStream(file));
         System.out.println("File Size : " + contents.length);
 
